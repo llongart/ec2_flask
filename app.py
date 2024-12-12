b490c33f6856e5fdc029fa8d9fdcbecc8d9cfe08
llongart <llongart413@gmail.com>
2024-12-12 18:53:52 -0400

    Feat: add app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello World!</p>"
+
+
+@app.route("/things")
+def things():
+    return jsonify({
+        "payload": ["foo", "bar", "baz", "echo"]
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True, host="localhost", port=5000)
